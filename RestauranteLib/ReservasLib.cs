97c844a7a23624214bc1f77eb20992d1053bb9d2
Jemima Weld <Jemima.Weld@studentambassadors.com>
2024-07-22 17:11:02 -0600

    Creacion de la base de datos: RestauranteDB

--- RestauranteLib/ReservasLib.cs
+++ RestauranteLib/ReservasLib.cs
@@ -2,45 +2,50 @@
 {
     public class ReservasLib
     {
-        private string _reservaID;
-        private string _reservaDate;
-        private string _reservaTime;
-        private string _clienteID;
-        private string _personasCant;
-        private string _numeroMesa;
+        private int _reservaID;
+        private DateOnly _reservaDate;
+        private TimeOnly _reservaTime;
+        private int _clienteID;
+        private int _personasCant;
+        private int _numeroMesa;
+        private string _reservaEstado;
 
-        public string ReservaID { get => _reservaID; set => _reservaID = value; }
-        public string ReservaDate { get => _reservaDate; set => _reservaDate = value; }
-        public string ReservaTime { get => _reservaTime; set => _reservaTime = value; }
-        public string ClienteID { get => _clienteID; set => _clienteID = value; }
-        public string PersonasCant { get => _personasCant; set => _personasCant = value; }
-        public string NumeroMesa { get => _numeroMesa; set => _numeroMesa = value; }
+        public int ReservaID { get => _reservaID; set => _reservaID = value; }
+        public DateOnly ReservaDate { get => _reservaDate; set => _reservaDate = value; }
+        public TimeOnly ReservaTime { get => _reservaTime; set => _reservaTime = value; }
+        public int ClienteID { get => _clienteID; set => _clienteID = value; }
+        public int PersonasCant { get => _personasCant; set => _personasCant = value; }
+        public int NumeroMesa { get => _numeroMesa; set => _numeroMesa = value; }
+        public string ReservaEstado { get => _reservaEstado; set => _reservaEstado = value; }
 
         public ReservasLib()
         {
-            _reservaID = "";
-            _reservaDate = "";
-            _reservaTime = "";
-            _clienteID = "";
-            _personasCant = "";
-            _numeroMesa = "";
+            ReservaID = 0;
+            ClienteID = 0;
+            PersonasCant = 0;
+            NumeroMesa = 0;
+            _reservaEstado = "";
         }
 
         public ReservasLib(
-            string reservaID,
-            string reservaDate,
-            string reservaTime,
-            string clienteID,
-            string personasCant,
-            string numeroMesa
+            int reservaID,
+            DateOnly reservaDate,
+            TimeOnly reservaTime,
+            int clienteID,
+            int personasCant,
+            int numeroMesa,
+            string reservaEstado
             )
         {
-            _reservaID = reservaID;
-            _reservaDate = reservaDate;
-            _reservaTime = reservaTime;
-            _clienteID = clienteID;
-            _personasCant = personasCant;
-            _numeroMesa = numeroMesa;
+            ReservaID = reservaID;
+            ReservaDate = reservaDate;
+            ReservaTime = reservaTime;
+            ClienteID = clienteID;
+            PersonasCant = personasCant;
+            NumeroMesa = numeroMesa;
+            _reservaEstado = reservaEstado;
         }
+
+       
     }
 }
